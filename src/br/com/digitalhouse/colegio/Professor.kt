1607f90605e611bf475aa11a2422e52bc6efc90c
Bruno Saunders <senabruno97@hotmail.com>
2020-09-14 22:05:24 -0300

    class professor

--- src/br/com/digitalhouse/colegio/Professor.kt
+++ src/br/com/digitalhouse/colegio/Professor.kt
@@ -1,4 +1,13 @@
 package br.com.digitalhouse.colegio
 
-class Professor {
+class Professor(numeroDocente, nome, sobrenome): Pessoa(nome, sobrenome), Docente {
+    override val registroDocente = numeroDocente
+
+    override fun darAula() {
+        println("Dando aula")
+    }
+
+    override fun fazerChamada() {
+        println("Fazendo chamada")
+    }
 }
